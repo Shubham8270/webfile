36f8a008f51577adeabd6c95f100d32784535e52
Shubham8270 <shubhamkumarjha8270@gmail.com>
2024-07-31 11:41:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import Login from './components/login';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route exact path="/" render={() => <Redirect to="/login" />} />
+      <Route path="/login" component={Login} />
+      <Route path="/app" component={App} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
