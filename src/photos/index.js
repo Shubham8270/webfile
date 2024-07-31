6444f69705ed98aa9d170fd1705eb0566169e590
Shubham8270 <shubhamkumarjha8270@gmail.com>
2024-07-31 06:04:12 +0000

    Updates

--- src/photos/index.js
+++
@@ -1,9 +0,0 @@
-const images = {
-    profile: require('./profile.jpg'),
-    product1: require('./product1.jpg'),
-    product2: require('./product2.jpg'),
-    // Add all product images similarly
-  };
-  
-  export default images;
-  
